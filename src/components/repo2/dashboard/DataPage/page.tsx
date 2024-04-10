ddf8a87861c0011811c5520cc8958faacefaca31
Sandadi Rithvik Reddy <srreddy@EX2INDIA.LOCAL>
2024-04-10 10:38:53 +0530

    changes to dashboard

---
+++ src/components/repo2/dashboard/DataPage/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TicketSystem from "@/components/repo2/dashboard/DashboardDataComponent/Data";
+
+const YourComponent = () => {
+  return (
+    <>
+      <TicketSystem />
+      <TicketSystem />
+    </>
+  );
+};
+
+export default YourComponent;
