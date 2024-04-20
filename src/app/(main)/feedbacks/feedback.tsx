5b20ec83a73bc38ac2fec993faf8b1286fcc529d
Mahammad Siraj. Cheruvu <mscheruvu@EX2INDIA.LOCAL>
2024-04-21 02:30:18 +0530

    completed feedbacks

---
+++ src/app/(main)/feedbacks/feedback.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { METHODS } from "http";
+import React, {useState} from "react";
+import Loading from "./loading";
+
+function Feedback({feedback}) {
+    const [viewed, setViewed] = useState(feedback.viewed)
+    const [marking, setMarking] = useState(false);
+    const {id, title, description, createdby, createdat} = feedback;
+
+    async function markRead() {
+        if(marking) return
+        try {
+            setMarking(true)
+            const url = "http://localhost:3000/api/feedbacks/admin/markviewed?id="+id;
+            await fetch(url, {
+                method: "PUT"
+            })
+            .then(res => setViewed(true))
+            .catch(err => {
+                setMarking(false)
+                console.log(err)
+            });
+        }catch(err) 
+        {
+            setMarking(false)
+            console.log(err);
+        }
+    }
+   return (
+    <div className="bg-white shadow-md rounded-md p-4 mb-4 w-4/6">
+    <div className="flex justify-between items-center mb-2">
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <p className="text-m text-black bg-slate-200 px-2 py-1 rounded-lg shadow-md">{createdby?createdby:"Anonymous"}</p>
+    </div>
+        <p className="text-base mb-2">{description}</p>
+        <div className="flex flex-row justify-between items-center">
+            <p className="text-sm text-gray-500">
+                {new Date(createdat).toLocaleString()}
+            </p>
+            {!viewed &&
+                <button 
+                disabled={marking}
+                onClick={markRead}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+                    {marking?"Marking...":"Mark as Viewed"}
+                </button>
+            || 
+                <p className="bg-green-500 text-white px-2 py-1 rounded-3xl">Viewed</p>
+            }
+        </div>
+</div>
+
+   )
+}
+
+export default Feedback;
---
+++ src/app/(main)/user/feedbacks/page.tsx
@@ -0,0 +1,163 @@
+"use client"
+import React, {use, useState} from "react";
+
+function Feedbacks() {
+  const [title, setTitle] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [isAnonymous, setIsAnonymous] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
+  const [conformation, setConformation] = useState(false);
+  const [ack, setAck] = useState("");
+  const [ackClass, setAckClass] = useState("text-red-700 font-semibold pt-1");
+  const [sending, setSending] = useState(false);
+
+  const handleSubmit = async () => {
+    console.log("run")
+    if(title.trim() === "") {
+      setAck("Title is empty");
+      setConformation(false);
+      return
+    }
+    if(feedback.trim() === "") {
+      setAck("Feedback is empty");
+      setConformation(false);
+      return
+    }
+    if(!conformation && !isAnonymous) {
+      setShowDialog(true);
+      return
+    }
+    const feedbackDTO = {
+      title: title,
+      feedback: feedback,
+      anonymous: isAnonymous
+    }
+    setSending(true);
+    const url = "http://localhost:3000/api/feedbacks/addfeedback";
+    try {
+      await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(feedbackDTO),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(result => {
+        console.log(result)
+        setAck("Feedback submitted successfully");
+        setAckClass("text-green-700 font-semibold pt-1")
+        setTitle("")
+        setFeedback("");
+        setIsAnonymous(false);
+        setConformation(false);
+        setSending(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setAck("Error while submitting feedback");
+        setConformation(false);
+        setAck("");
+        setSending(false);
+      })
+    }
+    catch(error) {
+        setAck("Error while submitting feedback");
+        setConformation(false);
+        setAck("");
+        setSending(false);
+    }
+  }
+
+  return (
+    <section>
+      {showDialog && !isAnonymous && !conformation && (
+        <div className="flex flex-col justify-center items-center bg-slate-500 bg-opacity-70 absolute left-0 right-0 top-0 bottom-0">
+        <div className="w-fit px-6 py-4 flex flex-col p-2 justify-center items-center max-w-sm bg-white">
+            <p className="px-2 py-4 text-lg font-bold text-center">
+                  Your feedback will not be anonymous. Are you sure you want to proceed?
+            </p>
+           <div className="flex flex-row gap-2">
+              <button onClick={() => {
+                  setShowDialog(false)
+                  setConformation(true)
+                }} 
+                className="bg-red-600 text-white px-4 py-2 max-w-48">
+                    yes, proceed
+                </button>
+                <button 
+                onClick={() => {
+                  setShowDialog(false)
+                  setIsAnonymous(true)
+                  setConformation(true)
+                }} 
+                className="bg-green-600 text-white px-4 py-2 max-w-40">
+                  Mark as Anonymous
+                </button>
+           </div>
+        </div>
+    </div>
+      )}
+      <div className="container">
+        <div className="flex w-full items-center py-12 flex-col">
+          <h2 className="mb-3 text-3xl font-bold text-black">
+            Feedback Form
+          </h2>
+          <p className="text-sm">We value your feedback!</p>
+          <form className="mt-5 text-start">
+                <input
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={e => {
+                    setTitle(e.target.value)
+                    setConformation(false)
+                    setAck("")
+                  }}
+                  placeholder="Enter the title"
+                  className="w-full rounded-md border-2 p-2"
+                  required
+                />
+                <textarea
+                  name="feedback"
+                  id="feedback"
+                  value={feedback}
+                  onChange={e => {
+                    setFeedback(e.target.value)
+                    setConformation(false)
+                    setAck("")
+                  }}
+                  cols={30}
+                  rows={10}
+                  className="mt-4 w-full rounded-md border-2 p-2"
+                  placeholder="Feedback"
+                  required
+                ></textarea>
+          </form>
+            <div className="mt-4 flex items-center">
+              <input
+                type="checkbox"
+                id="anonymous"
+                checked={isAnonymous}
+                onChange={() => {
+                  setIsAnonymous(!isAnonymous)
+                  setConformation(false)
+                  setAck("")
+                }}
+                className="mr-2 size-4"
+              />
+              <label htmlFor="anonymous" className="text-lg">Anonymous</label>
+            </div>
+            <p className={ackClass}>{ack}</p>
+          <button 
+            onClick={handleSubmit}
+            className="mt-2 bg-rose-600 px-4 py-2 text-white">
+            {sending?"Submitting":"Submit"}
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Feedbacks;
+
